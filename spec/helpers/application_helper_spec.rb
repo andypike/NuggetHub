abc59e9456c086c4e04b50446a4a2c644292a8fe
Andy Pike <andy@andypike.com>
2010-01-27 23:49:16 +0000

    Added tags controller to tag path in helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,17 +5,17 @@ describe ApplicationHelper do
   context "When generating the path for a tag" do
     it "should return an standard path for normal characters" do
       tag = Tag.new(:name => "normal")
-      helper.tag_path(tag).should == "normal"
+      helper.tag_path(tag).should == "/tags/normal"
     end
 
     it "should escape invalid url characters" do
       tag = Tag.new(:name => "c++")
-      helper.tag_path(tag).should == "c%2B%2B"
+      helper.tag_path(tag).should == "/tags/c%2B%2B"
     end
 
     it "should escape dot (.) characters" do
       tag = Tag.new(:name => ".NET")
-      helper.tag_path(tag).should == "%2ENET"
+      helper.tag_path(tag).should == "/tags/%2ENET"
     end
   end
 
