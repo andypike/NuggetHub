fc98dde2933d9277c687fa0fcc7e1a8848fb3e79
Andy Pike <andy@andypike.com>
2010-01-27 23:44:34 +0000

    Moved tag path formatting to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
-# Methods added to this helper will be available to all templates in the application.
+
 module ApplicationHelper
+  def tag_path(tag)
+    CGI::escape(tag.name).gsub(".", "%2E")
+  end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+
+  context "When generating the path for a tag" do
+    it "should return an standard path for normal characters" do
+      tag = Tag.new(:name => "normal")
+      helper.tag_path(tag).should == "normal"
+    end
+
+    it "should escape invalid url characters" do
+      tag = Tag.new(:name => "c++")
+      helper.tag_path(tag).should == "c%2B%2B"
+    end
+
+    it "should escape dot (.) characters" do
+      tag = Tag.new(:name => ".NET")
+      helper.tag_path(tag).should == "%2ENET"
+    end
+  end
+
+  it "should be included in the object returned by #helper" do
+    included_modules = (class << helper; self; end).send :included_modules
+    included_modules.should include(ApplicationHelper)
+  end
+
+end
